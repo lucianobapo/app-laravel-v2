9f5648b48544753771f9c0238dc162a58cdf33b8
Luciano Porto <luciano.bapo@gmail.com>
2015-05-29 05:42:13 -0300

    acertos

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -120,11 +120,14 @@ class DeliveryController extends Controller {
             $panelListaEnderecos = '';
             $panelGuest = view('delivery.partials.panelGuestLogin', compact('host'));
         } else {
+
+            $id = Partner::firstByAttributes([
+                'user_id' => Auth::user()->id,
+            ])->id;
+            dd($id);
             $panelListaEnderecos = view('delivery.partials.pedidoListEnderecos')->with([
                 'enderecos' => Address::where([
-                    'partner_id' => Partner::firstByAttributes([
-                        'user_id' => Auth::user()->id,
-                    ])->id,
+                    'partner_id' => $id,
                 ])->get(),
             ]);
             $panelGuest = '';
